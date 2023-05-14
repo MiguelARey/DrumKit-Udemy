3511de7e4e179af1b280ad59812a8effec60f5f7
MiguelAnaya <mianare.99.anaya@gmail.com>
2023-05-14 12:13:31 -0700

    se agrego el event listener para cuando se presione la tecla

--- script.js
+++ script.js
@@ -2,10 +2,53 @@
 //     alert("I got clicked!")
 // });
 
+// Detectar cuando la imagen se presione
 for(let i=0;i<7;i++){
     document.getElementsByTagName("button")[i].addEventListener("click",function handleClick(){
-        alert("I got clicked!")
+        let buttonInnerHtml = this.innerHTML
+        makeSound(buttonInnerHtml);
     });
+}
+
+// Detectar cuando la tecla se presione
+let keyHandler = document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+})
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            let kick = new Audio('/sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "a":
+            let tom1 = new Audio('/sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "s":
+            let tom4 = new Audio('/sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "d":
+            let tom3 = new Audio('/sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "j":
+            let tom2 = new Audio('/sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            let snare = new Audio('/sounds/snare.mp3');
+            snare.play();
+            break;
+        case "l":
+            let crash = new Audio('/sounds/crash.mp3');
+            crash.play();
+            break;
+    
+        default: console.log(buttonInnerHtml);
+            
+    }
 }
 
 
